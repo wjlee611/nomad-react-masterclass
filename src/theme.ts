4c6a767e0b72db87246724ccbd02a204019fcc42
wjlee611 <wjlee611@gmail.com>
2022-06-20 16:06:21 +0900

    protected theme

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "White",
+  textColor: "black",
+  btnColor: "teal",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "black",
+  textColor: "white",
+  btnColor: "tomato",
+};
