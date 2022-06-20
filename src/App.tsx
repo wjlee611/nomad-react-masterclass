0ad2ff98cee80c23be599a7ff018a30d15bb80f4
wjlee611 <wjlee611@gmail.com>
2022-06-20 15:53:06 +0900

    form event types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(`Hello ${value}`);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="#aaa" />
-      <Circle bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
